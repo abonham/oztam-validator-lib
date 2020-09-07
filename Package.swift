9df6a675f709fa5253c51fcdf5a96360383e9b81
Aaron Bonham <aaron.bonham@sbs.com.au>
2020-09-07 13:23:26 +1000

    name

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "OztamValidator",
+            name: "oztam-validator-lib",
             targets: ["oztam-validator-lib"]),
     ],
     dependencies: [
