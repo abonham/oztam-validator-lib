ba50a55fbfef1b38a52c0c3b48f2c2072227c382
Aaron Bonham <aaron.bonham@sbs.com.au>
2020-09-07 13:35:51 +1000

    name

--- Package.swift
+++ Package.swift
@@ -26,6 +26,6 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "oztam-validator-libTests",
-            dependencies: ["oztam-validator-lib"]),
+            dependencies: ["OztamValidatorLib"]),
     ]
 )
